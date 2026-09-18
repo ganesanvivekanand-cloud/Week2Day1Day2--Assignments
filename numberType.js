1648e5e029a474e93ceda57341795b7a23597e5e
ganesanvivekanand-cloud <ganesanvivekanand@gmail.com>
2026-09-18 08:25:02 +0530

    Add files via upload

---
+++ numberType.js
@@ -0,0 +1,24 @@
+
+let numtype=0;
+
+
+function numberType(){
+
+
+    if(numtype>=0){
+
+        console.log("Positive Number")
+    }
+    else if(numtype ===0){
+
+        console.log("Number is Neutral")
+    }
+    else
+    {
+        console.log("Negative Number")
+    }
+
+
+}
+
+numberType(numtype)
